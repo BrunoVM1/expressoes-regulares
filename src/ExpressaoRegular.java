0bd74205e836114cd829b835cc117d0321eb59a8
BrunoVM1 <bruno.venturim1@gmail.com>
2024-12-06 12:17:18 -0300

    testando para nomes

--- src/ExpressaoRegular.java
+++ src/ExpressaoRegular.java
@@ -1,2 +1,15 @@
 public class ExpressaoRegular {
+    public String LETRA = "[A-Za-z]";
+    public String LETRAS = LETRA + "*";
+    public String BRANCO = "\\s";
+    public String BRANCOS = BRANCO + "*";
+    public String NOME = "("+LETRAS+"|"+BRANCOS+")*";
+
+    public void confere(String exp, String sentenca){
+        if(sentenca.matches(exp)){
+            System.out.println("W:'" + sentenca + " ACEITA");
+        } else {
+            System.err.println("W:'" + sentenca + " REJEITADA");
+        }
+    }
 }
